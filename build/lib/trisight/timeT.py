c233c972c0c153f746bcba0dfa0a0330a1b4df09
Zidiefeng <ksun01@email.wm.edu>
2020-09-19 21:10:33 -0400

    update

---
+++ build/lib/trisight/timeT.py
@@ -0,0 +1,71 @@
+import datetime
+
+def stamp_to_dt(timestamp):
+    '''Transfer timestamp to date time
+
+    Use example:
+        .. code-block:: python
+
+            stamp_to_dt(1590988249)
+
+    :param timestamp: timestamp to be transformed
+    :type timestamp: timestamp
+
+    :returns: transformed date time
+    :rtype: datetime
+    '''
+
+    dt = datetime.datetime.fromtimestamp(timestamp)
+    return dt
+
+def str_to_dt(dt_str):
+    '''Transfer string format date time to date time
+
+    Use example:
+        .. code-block:: python
+
+            str_to_dt('2020-09-01 11:15:50')
+
+    :param dt_str: date time in string format, such as '2020-09-01 11:15:49' (%Y-%m-%d %H:%M:%S)
+    :type dt_str: str
+
+    :returns: transformed date time
+    :rtype: datetime
+    '''
+    dt = datetime.datetime.strptime(dt_str, '%Y-%m-%d %H:%M:%S')
+    return dt
+
+def dt_to_stamp(dt):
+    '''Transfer date time to timestamp
+
+    Use example:
+        .. code-block:: python
+
+            dt_to_stamp(datetime.now())
+
+    :param dt_str: date time, such as '2020-09-01 11:15:49' (%Y-%m-%d %H:%M:%S)
+    :type dt_str: str
+
+    :returns: transformed timestamp
+    :rtype: timestamp
+    '''
+
+    timestamp = datetime.datetime.timestamp(dt)
+    return timestamp
+
+def str_to_stamp(dt_str):
+    '''Transfer string to timestamp
+
+    Use example:
+        .. code-block:: python
+
+            str_to_stamp('2020-09-01 11:15:50')
+
+    :param dt_str: string date time, such as '2020-09-01 11:15:49' (%Y-%m-%d %H:%M:%S)
+    :type dt_str: str
+
+    :returns: transformed timestamp
+    :rtype: timestamp
+    '''
+    timestamp = dt_to_stamp(str_to_dt(dt_str))
+    return timestamp
--- trisight/financial.py
+++ trisight/financial.py
@@ -1,36 +1,35 @@
 import requests
+from trisight import token_check
 import pandas as pd
 pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
 
-def quarterly_estimate_actual(stock_code, from_date, to_date, token):
-    '''Get estimate vs. actual EPS and revenue of each quarterly report,
-    no info loss, no need to keep json
+token = token_check.get_token(token_check.token)
+
+def quarterly_estimate_actual(stock_code, from_date, to_date, token=token):
+    '''Get estimate vs. actual EPS and revenue of each quarterly report (no info loss)
 
     Use example:
         .. code-block:: python
 
             quarterly_estimate_actual("AAPL", "2019-08-31","2020-09-11","btapdan48v6stqoinlu0")
+            quarterly_estimate_actual("AAPL", "2019-08-31","2020-09-11")
 
-    :param stock_code:
-        stock code, ex. "AAPL"
-    :type stock_code:
-        str
+    :param stock_code: stock code, ex. "AAPL"
+    :type stock_code: str
 
-    :param from_date:
-        time string, ex."2020-09-11"
+    :param from_date: time string, ex."2020-09-11"
     :type from_date: str
 
-    :param to_date:
-        time string, ex."2020-09-11"
-    :type to_date:
-        str
+    :param to_date: time string, ex."2020-09-11"
+    :type to_date:str
+
+    :param token: token, optional, ex."btapdan48v6stqoinlu0"
+    :type token: str
 
 
-    :returns:
-        dataframe (row: report)
-    :rtype:
-        pd.DataFrame
+    :returns: dataframe (row: report)
+    :rtype: pd.DataFrame
     '''
 
     req=f'https://finnhub.io/api/v1/calendar/earnings?from={from_date}&to={to_date}&symbol={stock_code}&token={token}'
