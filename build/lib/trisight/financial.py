5d9758c118de4b9e676b95d533c87b5f26067fcd
Zidiefeng <ksun01@email.wm.edu>
2020-09-29 22:06:14 -0400

    add extract next release date function

--- build/lib/trisight/financial.py
+++ build/lib/trisight/financial.py
@@ -2,11 +2,47 @@ import requests
 from trisight import token_check
 from trisight import timeT as t
 import datetime
+from dateutil.relativedelta import *
+import datetime as dt
 import pandas as pd
 pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
 
 token = token_check.get_token(token_check.token)
+
+
+def next_release_date(stock_code, n_months=3, token=token):
+    '''Get next release date (string) within n months from today
+
+    Use example:
+        .. code-block:: python
+
+            next_release_date("AAPL",n_months=1)
+
+    :param stock_code: stock code, ex. "AAPL"
+    :type stock_code: str
+
+    :param n_months: how many months ahead to search release date (defualt=3)
+    :type n_months: int
+
+    :param token: token, optional, ex."btapdan48v6stqoinlu0"
+    :type token: str
+
+    :returns: string date (if exists), False (if not exists)
+    :rtype: str
+    '''
+    now = dt.datetime.now()
+    months_later = now+relativedelta(months=+n_months)
+    str_today = now.strftime("%Y-%m-%d")
+    str_m_after = months_later.strftime("%Y-%m-%d")
+    df=quarterly_estimate_actual(stock_code, now, str_m_after, token=token)
+    if df.shape[0]==0:
+        print(f"No {stock_code} future release found within {n_months} months from today")
+        result= False
+    else:
+        result= df.date.iloc[0]
+    return result
+
 
 def quarterly_estimate_actual(stock_code, from_date, to_date, token=token):
     '''Get estimate vs. actual EPS and revenue of each quarterly report (no info loss)
@@ -120,8 +156,8 @@ def extract_last_file_date(stock_code, target_datetime, restrict_to_90d = True, 
 
 
 def extract_recent_reports(stock_code,target_datetime,restrict_to_90d = True, token=token):
-    
-    '''Get the most recent report dict of a company right before target date time within 90d/1y
+
+    '''Get the most recent report dict of a company <= target date time (within 90d/1y)
 
     Use example:
         .. code-block:: python
@@ -141,11 +177,11 @@ def extract_recent_reports(stock_code,target_datetime,restrict_to_90d = True, to
     :param token: token, optional, ex."btapdan48v6stqoinlu0"
     :type token: str
 
-    :returns: recent report dict (keys: ["balanceSheet","incomeStatement","cashFlow"], each can extract a dataframe)
+    :returns: recent report dict (keys: ["balanceSheet","incomeStatement","cashFlow", ...], each can extract a dataframe)
     :rtype: dict
 
     '''
-    
+
     reports = extract_reports(stock_code,token)
     file_date = extract_last_file_date(stock_code, target_datetime, restrict_to_90d=restrict_to_90d, token=token)
     if file_date==False:
@@ -160,7 +196,14 @@ def extract_recent_reports(stock_code,target_datetime,restrict_to_90d = True, to
             print(f"No income statement available for {stock_code} at {file_date}!")
         if cf.shape[0] == 0:
             print(f"No cash flow report available for {stock_code} at {file_date}!")
-        reports_dict = {"balanceSheet": bs,
+        reports_dict = {'symbol':reports.loc[file_date]['symbol'],
+                        'year':reports.loc[file_date]['year'],
+                        'quarter':reports.loc[file_date]['quarter'],
+                        'form':reports.loc[file_date]['form'],
+                        'startDate':reports.loc[file_date]['startDate'],
+                        'endDate':reports.loc[file_date]['endDate'],
+                        'fileDate':file_date,
+                        "balanceSheet": bs,
                         "incomeStatement": ic,
                         "cashFlow": cf}
         return reports_dict
